8c95cc893224d4f37f5dcd68a1a9c29641497cc9
wjdtj9656 <wjdtj9656@naver.com>
2021-09-03 18:45:10 +0900

    내 풀이

--- chap8 다이나믹 프로그래밍/바닥 공사/Main.java
+++ chap8 다이나믹 프로그래밍/바닥 공사/Main.java
@@ -13,7 +13,7 @@ public class Main {
     arr[0] = 1;
     arr[1] = 1;
     for(int i=2; i<=num; i++) {
-    	arr[i] = arr[i-2]*2 + arr[i-1];
+    	arr[i] = (arr[i-2]*2 + arr[i-1])%796796;
     }
     bw.write(String.valueOf(arr[num]));
     br.close();
