529bd49cec72132a4efbe69c1f3d2d5969be9030
wjdtj9656 <wjdtj9656@naver.com>
2021-09-07 22:12:34 +0900

    내 풀이

--- chap11 그리디 문제/모험가 길드/Main.java
+++ chap11 그리디 문제/모험가 길드/Main.java
@@ -8,16 +8,21 @@ public class Main {
         
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Integer arr[] = new Integer[N];
+        int arr[] = new int[N];
         for(int i=0; i<N; i++) {
         	arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr,Collections.reverseOrder());
+        Arrays.sort(arr);
         int count = 0;
+        int result = 0;
         for(int i=0; i<N;i+=arr[i]) {
         	count++;
+        	if(count >= arr[i]) {
+        		result++;
+        		count = 0;
+        	}
         }
-        bw.write(String.valueOf(count));
+        bw.write(String.valueOf(result));
         br.close();
         bw.flush();
         bw.close();
